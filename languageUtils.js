68041590d99428cbd996f2417c3593447e4c7735
Alan Trebugeais <alan.trebugeais@epitech.eu>
2024-07-03 15:27:02 +0200

    [ADD] language en fr

--- languageUtils.js
+++ languageUtils.js
@@ -17,51 +17,76 @@ export function updateFlag(lang) {
 
 export function changeLanguage(lang) {
     console.log('Changing language to:', lang);
-    // Try fetching using the local path first
     fetch(`translations/${lang}.json`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Local fetch failed');
-            }
-            return response.json();
-        })
+    //fetch(`/ExtraBinoss.github.io/translations/${lang}.json`)
+        .then(response => response.json())
         .then(translations => {
-            applyTranslations(translations);
-        })
-        .catch(error => {
-            console.error('Error loading the local translation file:', error);
-            // If the local fetch fails, try fetching from the GitHub Pages path
-            fetch(`/ExtraBinoss.github.io/translations/${lang}.json`)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('GitHub Pages fetch failed');
+            document.querySelectorAll('[data-translate-key]').forEach(element => {
+                const keys = element.getAttribute('data-translate-key').split('.');
+                let translation = translations;
+                keys.forEach(key => {
+                    if (translation[key]) {
+                        translation = translation[key];
+                    } else {
+                        translation = null;
+                        return;
                     }
-                    return response.json();
-                })
-                .then(translations => {
-                    applyTranslations(translations);
-                })
-                .catch(error => console.error('Error loading the GitHub Pages translation file:', error));
-        });
+                });
+                if (translation) {
+                    element.innerHTML = translation;
+                }
+            });
+        })
+        .catch(error => console.error('Error loading the translation file:', error));
 }
 
-function applyTranslations(translations) {
-    document.querySelectorAll('[data-translate-key]').forEach(element => {
-        const keys = element.getAttribute('data-translate-key').split('.');
-        let translation = translations;
-        keys.forEach(key => {
-            if (translation[key]) {
-                translation = translation[key];
-            } else {
-                translation = null;
-                return;
-            }
-        });
-        if (translation) {
-            element.innerHTML = translation;
-        }
-    });
-}
+// export function changeLanguage(lang) {
+//     console.log('Changing language to:', lang);
+//     // Try fetching using the local path first
+//     fetch(`translations/${lang}.json`)
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error('Local fetch failed');
+//             }
+//             return response.json();
+//         })
+//         .then(translations => {
+//             applyTranslations(translations);
+//         })
+//         .catch(error => {
+//             console.error('Error loading the local translation file:', error);
+//             // If the local fetch fails, try fetching from the GitHub Pages path
+//             fetch(`/ExtraBinoss.github.io/translations/${lang}.json`)
+//                 .then(response => {
+//                     if (!response.ok) {
+//                         throw new Error('GitHub Pages fetch failed');
+//                     }
+//                     return response.json();
+//                 })
+//                 .then(translations => {
+//                     applyTranslations(translations);
+//                 })
+//                 .catch(error => console.error('Error loading the GitHub Pages translation file:', error));
+//         });
+// }
+
+// function applyTranslations(translations) {
+//     document.querySelectorAll('[data-translate-key]').forEach(element => {
+//         const keys = element.getAttribute('data-translate-key').split('.');
+//         let translation = translations;
+//         keys.forEach(key => {
+//             if (translation[key]) {
+//                 translation = translation[key];
+//             } else {
+//                 translation = null;
+//                 return;
+//             }
+//         });
+//         if (translation) {
+//             element.innerHTML = translation;
+//         }
+//     });
+// }
 export function setLanguageByBrowser() {
     const userLang = navigator.language || navigator.userLanguage; // Get browser's language
     let lang = userLang.startsWith('fr') ? 'fr' : 'en'; // Default to English if not French
